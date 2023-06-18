889b29d0ca40cd55d83dfe3e0896177efb739ef1
Josue Freitas <josue1912@gmail.com>
2023-06-18 13:47:03 -0300

    Passando parâmetro para a tasklet após a configuração

--- primeiroprojetospringbatch/src/main/java/com/example/primeiroprojetospringbatch/job/ImprimeOlaTasklet.java
+++ primeiroprojetospringbatch/src/main/java/com/example/primeiroprojetospringbatch/job/ImprimeOlaTasklet.java
@@ -1,17 +1,21 @@
 package com.example.primeiroprojetospringbatch.job;
 
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component public class ImprimeOlaTasklet implements Tasklet {
+@Component @StepScope public class ImprimeOlaTasklet implements Tasklet {
+
+  @Value("#{jobParameters['nome']}") private String nome;
 
   @Override
   public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext)
       throws Exception {
-    System.out.println("Olá, mundo!");
+    System.out.printf("Olá, %s!%n", nome);
     return RepeatStatus.FINISHED;
   }
 }
