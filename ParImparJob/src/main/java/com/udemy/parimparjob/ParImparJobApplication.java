5277037727d0dea5dd3d2412745ec3d9b3528121
Josue Freitas <josue1912@gmail.com>
2023-06-18 13:31:44 -0300

    Criando step chunk-based e definindo commit interval

---
+++ ParImparJob/src/main/java/com/udemy/parimparjob/ParImparJobApplication.java
@@ -0,0 +1,12 @@
+package com.udemy.parimparjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class ParImparJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParImparJobApplication.class, args);
+	}
+
+}
