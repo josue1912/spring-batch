0deccfb42fd80816872c25a6341b6d7bd9037f85
Josue Freitas <josue1912@gmail.com>
2023-06-22 10:13:22 -0300

    Lendo arquivos com largura fixa

--- ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/reader/LeituraArquivoLarguraFixaReaderConfig.java
+++ ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/reader/LeituraArquivoLarguraFixaReaderConfig.java
@@ -1,16 +1,30 @@
 package com.springbatch.arquivolargurafixa.reader;
 
 import com.springbatch.arquivolargurafixa.dominio.Cliente;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.transform.Range;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class LeituraArquivoLarguraFixaReaderConfig {
 
-	@Bean
-	public FlatFileItemReader<Cliente> leituraArquivoLarguraFixaReader() {
-		//TODO: Implementar leitor de arquivo de largura fixa.
-		return null;
-	}
+    @Bean
+    @StepScope
+    public FlatFileItemReader<Cliente> leituraArquivoLarguraFixaReader(
+            @Value("#{jobParameters['arquivo']}") Resource arquivo) {
+        return new FlatFileItemReaderBuilder<Cliente>()
+                .name("leituraArquivoLarguraFixaReader")
+                .resource(arquivo)
+                .fixedLength()
+                .columns(new Range[]{new Range(1, 10), new Range(11, 20), new Range(21, 23), new Range(24)})
+                .names("nome", "sobrenome", "idade", "email")
+                .targetType(Cliente.class)
+                .build();
+
+    }
 }
